138ce521c7ff6dce21827e89a4f8796e2cf4a762
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-03-13 16:44:45 -0500

    Creando la ruta del nuevo componente crearCliente

--- p12proyectoCrud/src/app/app.routes.ts
+++ p12proyectoCrud/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
 import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
 import { InicioComponent } from './inicio/inicio.component';
 
 export const routes: Routes = [
   {path: '', component: InicioComponent},
-  {path: 'clientes', component: ListadoClientesComponent}
+  {path: 'clientes', component: ListadoClientesComponent},
+  {path: 'crear-cliente', component: CrearClienteComponent}
 ];
