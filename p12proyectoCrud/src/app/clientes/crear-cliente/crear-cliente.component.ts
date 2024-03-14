13cc20001c03223144571a610100980116b039c5
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-03-14 16:46:51 -0500

    Copiando el fichero crear-cliente.component.ts en donde básicamente esta la lógica y las validaciones de cada campo del formulario y lo que hace es devolver o mostrar los resultado por medio del método get()  el cual retorna un JSON

--- p12proyectoCrud/src/app/clientes/crear-cliente/crear-cliente.component.ts
+++ p12proyectoCrud/src/app/clientes/crear-cliente/crear-cliente.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-crear-cliente',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './crear-cliente.component.html',
   styleUrl: './crear-cliente.component.css'
 })
-export class CrearClienteComponent {
+export class CrearClienteComponent implements OnInit {
 
+  // No inicializamos nuestra variable
+  form: FormGroup;
+
+  constructor(){ }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      nombre: new FormControl('', [Validators.required]),
+      cif: new FormControl('', [Validators.required, Validators.minLength(9)]),
+      direccion: new FormControl(''),
+      localidad: new FormControl('')
+    })
+  }
+
+  getValorJson(){
+    return JSON.stringify(this.form.value);
+  }
+
+  enviarCliente(): void {
+    console.log(this.form.value);
+  }
 }
