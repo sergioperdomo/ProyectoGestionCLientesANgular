0611447d1ce4ee2cf8264ca5cb877bb20e8a50f6
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-03-17 18:02:32 -0500

    Por medio del constructor llamamos la clase Router, y el servicio ClientesService para hacer uso de los mismo, aquí básicamente lo que se hace es que una vez se de clic en el boton acutalizar del cliente que seleccionemos, nos mostrara los campos del formularios, es decir, los imputs con la información que ya tiene registrada, por tanto, podemos cambiar la información que querramos, ya sea solo un campo o todos, y eso el cambio que se hizo se ve reflejado en la tabla en donde se encuentran los clientes

--- p12proyectoCrud/src/app/clientes/actualizar-cliente/actualizar-cliente.component.ts
+++ p12proyectoCrud/src/app/clientes/actualizar-cliente/actualizar-cliente.component.ts
@@ -1,28 +1,43 @@
+import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ClientesService } from '../../servicios/clientes.service';
 
 @Component({
   selector: 'app-actualizar-cliente',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf, RouterLink],
   templateUrl: './actualizar-cliente.component.html',
   styleUrl: './actualizar-cliente.component.css'
 })
 export class ActualizarClienteComponent implements OnInit {
 
   cif: string;
+  form: FormGroup;
+  cliente: any;
 
 
-  constructor(private route: ActivatedRoute, private clienteService: ClientesService){}
+  constructor(private route: ActivatedRoute, private router: Router, private clienteService: ClientesService){}
 
   ngOnInit(): void {
     this.cif = this.route.snapshot.params['cif'];
-    this.clienteService.getCliente(this.cif)
+    this.clienteService.getCliente(this.cif).subscribe((cliente) => {
+      this.form.patchValue(cliente);
+    })
+    this.form = new FormGroup({
+      nombre: new FormControl('', [Validators.required]),
+      cif: new FormControl('', [Validators.required, Validators.minLength(9)]),
+      direccion: new FormControl(''),
+      localidad: new FormControl('')
+    })
+  }
+
+  modificarCliente(){
+    this.clienteService.putClientes(this.form.value, this.cif)
                         .subscribe({
-                          next: (res: any) => {
-                            // this.cif = res;
-                            console.log(res);
+                          next: () => {
+                            this.router.navigate(['/clientes']);
                           },
                           error: (err: any) => {
                             console.log(err);}
