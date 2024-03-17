53a6d0c254e91b8c98c32df78e9b6d8fae0d69bb
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-03-17 14:47:19 -0500

    Agregandole una ruta a nuestro componente ActualizarCliente

--- p12proyectoCrud/src/app/app.routes.ts
+++ p12proyectoCrud/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { ActualizarClienteComponent } from './clientes/actualizar-cliente/actualizar-cliente.component';
 import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
 import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
 import { InicioComponent } from './inicio/inicio.component';
@@ -6,5 +7,6 @@ import { InicioComponent } from './inicio/inicio.component';
 export const routes: Routes = [
   {path: '', component: InicioComponent},
   {path: 'clientes', component: ListadoClientesComponent},
-  {path: 'crear-cliente', component: CrearClienteComponent}
+  {path: 'crear-cliente', component: CrearClienteComponent},
+  {path: 'actualizar-cliente/:cif', component: ActualizarClienteComponent}
 ];
